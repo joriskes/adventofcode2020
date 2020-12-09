9bba095e430ad30ca723561762c3cd8a9d5f4e4c
Joris <49513527+joriskes@users.noreply.github.com>
2020-12-09 10:25:20 +0100

    Day 8 part 1

--- src/day08.rs
+++ src/day08.rs
@@ -7,6 +7,95 @@ pub fn run(input_filename: &str) {
     part2(&input);
 }
 
-fn part1(_input: &String) {}
+#[derive(Debug)] // Added so you can debug print this enum
+enum OperationType {
+    Nop,
+    Acc,
+    Jmp,
+}
+
+#[allow(dead_code)]
+struct Instruction {
+    line: i32,
+    operation: OperationType,
+    number: i32,
+}
+
+impl std::fmt::Debug for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+        f.debug_struct("Bag")
+            .field("token", &self.operation)
+            .field("number", &self.number)
+            .finish()
+    }
+}
+
+fn string_to_operation_type(in_str: &str) -> OperationType {
+    let operation;
+    match in_str.to_lowercase().as_ref() {
+        "acc" => operation = OperationType::Acc,
+        "jmp" => operation = OperationType::Jmp,
+        "nop" => operation = OperationType::Nop,
+        _ => {
+            println!(
+                "Unkown operation {}, falling back to nop",
+                in_str.to_lowercase()
+            );
+            operation = OperationType::Nop;
+        }
+    }
+    return operation;
+}
+
+fn read_operation(line_number: i32, line: &str) -> Instruction {
+    let parts: Vec<&str> = line.trim().split(' ').collect();
+    assert_eq!(parts.len(), 2, "Invalid input received");
+    let operation = string_to_operation_type(parts[0]);
+
+    return Instruction {
+        line: line_number,
+        operation: operation,
+        number: parts[1].parse().unwrap(),
+    };
+}
+
+fn part1(input: &String) {
+    let mut line_number: i32 = 0;
+    let mut instructions: Vec<Instruction> = vec![];
+    for line_str in input.lines() {
+        instructions.push(read_operation(line_number, line_str));
+        line_number += 1;
+    }
+
+    let mut acc = 0;
+    let mut line: i32 = 0;
+    let mut executed: Vec<i32> = vec![];
+    loop {
+        let instruction = &instructions[line as usize];
+        executed.push(line);
+        match instruction.operation {
+            OperationType::Nop => {
+                line += 1;
+            }
+            OperationType::Acc => {
+                acc += instruction.number;
+                line += 1;
+            }
+            OperationType::Jmp => {
+                line += instruction.number;
+            }
+        }
+
+        if line < 0 || line > instructions.len() as i32 {
+            println!("Line number went outside of program");
+            break;
+        }
+
+        if executed.contains(&line) {
+            println!("Part 1: {}", acc);
+            break;
+        }
+    }
+}
 
 fn part2(_input: &String) {}
