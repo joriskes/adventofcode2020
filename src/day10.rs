fc900be483055ab59eed6def419048f98ccc687e
Joris <49513527+joriskes@users.noreply.github.com>
2020-12-11 08:57:10 +0100

    Day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,4 +1,5 @@
 use crate::aoc_utils::{input_into_numbers, read_input};
+use std::u32::MAX;
 
 pub fn run(input_filename: &str) {
     let input = read_input(input_filename);
@@ -30,4 +31,28 @@ fn part1(input: &Vec<u32>) {
     println!("Part 1 {}", diff_list[0] * diff_list[2]);
 }
 
-fn part2(_input: &Vec<u32>) {}
+fn part2(input: &Vec<u32>) {
+    let mut sorted = input.clone();
+    // Add start jolts
+    sorted.push(0);
+    sorted.sort();
+    // Add final jolts
+    sorted.push(sorted.get(sorted.len() - 1).unwrap() + 3);
+
+    let mut path_count: Vec<u64> = vec![];
+    for _p in 0..sorted.len() {
+        path_count.push(0);
+    }
+    path_count[0] = 1;
+    for (index, jolt) in sorted.iter().enumerate() {
+        for i in 1..4 {
+            if index + i < sorted.len() {
+                let next = sorted.get(index + i).unwrap_or(&MAX);
+                if next < &MAX && next - jolt <= 3 {
+                    path_count[index + i] += path_count[index];
+                }
+            }
+        }
+    }
+    println!("Part 2 {}", path_count.pop().unwrap());
+}
