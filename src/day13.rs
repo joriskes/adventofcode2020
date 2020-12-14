cf66b1b6c928e2a89b0a14b4d3482c978eb32806
Joris <49513527+joriskes@users.noreply.github.com>
2020-12-14 11:02:31 +0100

    day 13 part 2: mathy solution

--- src/day13.rs
+++ src/day13.rs
@@ -1,8 +1,8 @@
 use crate::aoc_utils::read_input;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Instant;
+// use std::collections::HashMap;
+// use std::sync::{Arc, Mutex};
+// use std::thread;
+// use std::time::Instant;
 
 pub fn run(input_filename: &str) {
     let input = read_input(input_filename);
@@ -37,92 +37,116 @@ fn part1(input: &String) {
     )
 }
 
-fn calc_timestamps(
-    from: u64,
-    to: u64,
-    offsets: HashMap<u64, u64>,
-    step_size: u64,
-    part2_mutex: Arc<Mutex<u64>>,
-) {
-    let mut timestamp: u64 = from;
-    loop {
-        let mut ok_count = 0;
-        for (line, index_offset) in offsets.iter() {
-            let num = (timestamp + index_offset) / line; // number is floored we need ceil
-            if line * num == timestamp + index_offset {
-                ok_count += 1;
-            } else {
-                break;
-            }
-        }
-        if ok_count == offsets.len() {
-            // We found a result! Set it in the mutex
-            let mut result = part2_mutex.lock().unwrap();
-            // We're looking for the smallest result
-            if *result == 0 || timestamp < *result {
-                println!("Result! {} {}", timestamp, result);
-                *result = timestamp
-            }
-            break;
-        }
-        timestamp += step_size;
-        if timestamp > to {
-            break;
-        }
-    }
-}
-
 fn part2(input: &String) {
-    let start = Instant::now();
-    let mut offsets: HashMap<u64, u64> = HashMap::new();
-    let mut step_num = 0;
     let lines: Vec<&str> = input.lines().nth(1).unwrap().split(",").collect();
     let mut index_offset = 0;
+    let mut res = 0;
+    let mut step = 1;
     for line in lines {
         if line != "x" {
             let line_num = line.parse::<u64>().unwrap();
-            offsets.insert(line_num, index_offset);
-            if step_num == 0 {
-                step_num = line_num;
+            loop {
+                if (res + index_offset) % line_num == 0 {
+                    step *= line_num;
+                    break;
+                }
+                res += step;
             }
+            println!("Line {} done, {}", line_num, index_offset);
         }
         index_offset += 1;
     }
 
-    let part2: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
-    let mut thread_offsets = 0;
-    let thread_count = 30;
-    let thread_size = step_num * 100000000;
-    loop {
-        let mut handles = vec![];
-        println!(
-            "Calculating from {} ({:.2?})",
-            thread_size * thread_offsets,
-            start.elapsed()
-        );
-
-        for i in 0..thread_count {
-            let off = offsets.clone();
-            let part2_mut = Arc::clone(&part2);
-            let handle = thread::spawn(move || {
-                let from = thread_size * (i + thread_offsets);
-                let to = from + thread_size - 1;
-                calc_timestamps(from, to, off, step_num, part2_mut);
-            });
-            handles.push(handle);
-        }
-        // Wait on all threads to finish
-        for handle in handles {
-            handle.join().unwrap();
-        }
-        if *part2.lock().unwrap() > 0 {
-            break;
-        }
-        thread_offsets += thread_count;
-    }
-    println!(
-        "Part 2: {} ({:.2?})",
-        *part2.lock().unwrap(),
-        start.elapsed()
-    );
+    println!("Part 2: {}", res);
 }
+
+//
+// fn calc_timestamps(
+//     from: u64,
+//     to: u64,
+//     offsets: HashMap<u64, u64>,
+//     step_size: u64,
+//     part2_mutex: Arc<Mutex<u64>>,
+// ) {
+//     let mut timestamp: u64 = from;
+//     loop {
+//         let mut ok_count = 0;
+//         for (line, index_offset) in offsets.iter() {
+//             let num = (timestamp + index_offset) / line; // number is floored we need ceil
+//             if line * num == timestamp + index_offset {
+//                 ok_count += 1;
+//             } else {
+//                 break;
+//             }
+//         }
+//         if ok_count == offsets.len() {
+//             // We found a result! Set it in the mutex
+//             let mut result = part2_mutex.lock().unwrap();
+//             // We're looking for the smallest result
+//             if *result == 0 || timestamp < *result {
+//                 println!("Result! {} {}", timestamp, result);
+//                 *result = timestamp
+//             }
+//             break;
+//         }
+//         timestamp += step_size;
+//         if timestamp > to {
+//             break;
+//         }
+//     }
+// }
+//
+// fn part2(input: &String) {
+//     let start = Instant::now();
+//     let mut offsets: HashMap<u64, u64> = HashMap::new();
+//     let mut step_num = 0;
+//     let lines: Vec<&str> = input.lines().nth(1).unwrap().split(",").collect();
+//     let mut index_offset = 0;
+//     for line in lines {
+//         if line != "x" {
+//             let line_num = line.parse::<u64>().unwrap();
+//             offsets.insert(line_num, index_offset);
+//             if step_num == 0 {
+//                 step_num = line_num;
+//             }
+//         }
+//         index_offset += 1;
+//     }
+//
+//     let part2: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
+//     let mut thread_offsets = 0;
+//     let thread_count = 30;
+//     let thread_size = step_num * 100000000;
+//     loop {
+//         println!(
+//             "Calculating from {} ({:.2?})",
+//             thread_size * thread_offsets,
+//             start.elapsed()
+//         );
+//
+//         let mut handles = vec![];
+//         for i in 0..thread_count {
+//             let off = offsets.clone();
+//             let part2_mut = Arc::clone(&part2);
+//             let handle = thread::spawn(move || {
+//                 let from = thread_size * (i + thread_offsets);
+//                 let to = from + thread_size - 1;
+//                 calc_timestamps(from, to, off, step_num, part2_mut);
+//             });
+//             handles.push(handle);
+//         }
+//         // Wait on all threads to finish
+//         for handle in handles {
+//             handle.join().unwrap();
+//         }
+//         if *part2.lock().unwrap() > 0 {
+//             break;
+//         }
+//         thread_offsets += thread_count;
+//     }
+//     println!(
+//         "Part 2: {} ({:.2?})",
+//         *part2.lock().unwrap(),
+//         start.elapsed()
+//     );
+// }
